611bb787b54677ac8338623aad54f57e34d37aa5
salah alhashmi <alhashmis246@gmail.com>
2021-10-26 21:51:25 +0400

    added

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
+// first : I change the import script tag to use defer mode
 
 
+//the question how to solve it
+// in my humble opinion , you want to use 2 thing:
+// DOM traversal (to go up and up to the first parent, and down to childs and childs sibling ......)
+// u
+// use recursion function, how to use:
+//
