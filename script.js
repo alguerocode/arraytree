632ec6b97622ce7a6c226de360e487a3db418540
salah alhashmi <alhashmis246@gmail.com>
2021-10-27 12:56:40 +0400

    added

--- script.js
+++ script.js
@@ -1,9 +1,72 @@
 // first : I change the import script tag to use defer mode
+// learn more about defer: https://bitsofco.de/async-vs-defer/
 
+// the plan to solve it:
+// select all checkbox and listen to it
+// when
+// run function traversal the parents elements and add minus icon
+// run function to traversal the childs elements and add tick icon
+// and reverse that when uncheckbox
 
-//the question how to solve it
-// in my humble opinion , you want to use 2 thing:
-// DOM traversal (to go up and up to the first parent, and down to childs and childs sibling ......)
-// u
-// use recursion function, how to use:
-//
+const checkboxElements = document.querySelectorAll("input[type='checkbox']");
+
+function traversalParents(target, checkOrUncheck) {
+  let targetLevel = parseInt(target.getAttribute("level"));
+  let curElement = target.parentElement;
+
+  // select the correct parent element by using level attribute
+  for (let i = 0; i < targetLevel; i++) {
+    curElement = curElement.parentElement;
+  }
+  // cur element !== null
+  while (curElement) {
+    curElement = curElement.previousElementSibling;
+    if (curElement) {
+      const previousSiblingInput = curElement.querySelector("input[type='checkbox']");
+      const previousLevel = parseInt(previousSiblingInput.getAttribute("level"));
+
+      if (targetLevel > previousLevel) {
+        previousSiblingInput.indeterminate = checkOrUncheck;
+        targetLevel = previousLevel;
+      }
+    }
+  }
+}
+function traversalChilds(target, checkOrUncheck) {
+  let targetLevel = parseInt(target.getAttribute("level"));
+  let curElement = target.parentElement;
+
+  // select the correct parent element by using level attribute
+  for (let i = 0; i < targetLevel; i++) {
+    curElement = curElement.parentElement;
+  }
+
+  while (curElement) {
+    curElement = curElement.nextElementSibling;
+    if (curElement) {
+      const previousSiblingInput = curElement.querySelector("input[type='checkbox']");
+      const previousLevel = parseInt(previousSiblingInput.getAttribute("level"));
+
+      if (targetLevel < previousLevel) {
+        previousSiblingInput.checked = checkOrUncheck;
+      } else {
+        break;
+      }
+    }
+  }
+}
+
+checkboxElements.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    if (e.target.checked) {
+      traversalParents(e.target, true);
+      traversalChilds(e.target, true);
+      console.log(e.target.getAttribute("level"));
+    } else {
+      traversalParents(e.target, false);
+      traversalChilds(e.target, false);
+    }
+  });
+});
+
+// backgound colo effect;
